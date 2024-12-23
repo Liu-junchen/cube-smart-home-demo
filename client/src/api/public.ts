f73d4896e7b283a59370dc9e8d937f6b603e6298
junchen.liu <junchen.liu@coolkit.cn>
2024-12-23 21:06:36 +0800

    feat: 删除多余的代码

--- client/src/api/public.ts
+++ client/src/api/public.ts
@@ -22,17 +22,6 @@ export async function request<T>(url: string, params: object, methodType: EReqMe
         const result = await axios(axiosConfig);
         return result ? result.data as IResponse<T> : {} as IResponse<T>;
     } catch (error) {
-
-        // 报错中包含网络错误，即后端接口失效
-        // const etcStore = useEtcStore();
-        const errorStr = JSON.stringify(error);
-        if (errorStr.includes('Network Error') || errorStr.includes('code 502')) {
-            // etcStore.setIsLoading(true)
-            // window.location.reload();
-            // TODO: 此处需要重连 websocket
-            // useDisconnect().startReconnect();
-        }
-
         if (axios.isAxiosError(error)) {
             console.log('http request axios error: ', error.code, error.message);
             // 超时错误返回特定的msg
